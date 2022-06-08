6a3484d69c9b55ebace73c230ef34ffcd32c800a
foratmp <106734322+foratmp@users.noreply.github.com>
2022-06-08 10:01:30 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,22 +1,45 @@
+let ASTEROID: Sprite = null
 game.splash("BENVINGUTS A L'ESPAI", "Apreta A per començar i B per disperar")
 effects.starField.startScreenEffect()
 let mySprite = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . 2 2 2 2 . . . . . . 
-    . . . . . . 2 f f 2 . . . . . . 
-    . . . . . . . c 5 . . . . . . . 
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c b . . . . . . . 
     . . . . . . . f f . . . . . . . 
-    . . . . . . . e 4 . . . . . . . 
-    . . . . . . e e 5 2 . . . . . . 
-    . . . 5 e 5 e 4 5 2 5 e 5 . . . 
-    . . 5 e e c c c 2 2 2 e e 5 . . 
-    . 5 e e 2 2 4 5 5 5 2 2 e e 5 . 
-    . e e f f f c c 2 2 f f 2 2 e . 
-    . e 2 2 2 2 2 4 4 5 4 5 4 2 e . 
-    . e 2 e 2 2 2 2 4 4 5 5 4 4 2 . 
-    . e 2 2 2 2 2 2 4 4 4 5 5 4 2 . 
+    . . . . . . . c 3 . . . . . . . 
+    . . . . . . . f f . . . . . . . 
+    . . . . . . . 8 3 . . . . . . . 
+    . . . . . . 8 8 1 a . . . . . . 
+    . . . . . . 8 3 1 a . . . . . . 
+    . . . . . c c c a a a . . . . . 
+    . . . . 8 8 3 3 3 1 a a . . . . 
+    . . 8 f f f c c a a f f a a . . 
+    . 8 8 8 8 a a 3 3 3 3 1 3 a a . 
+    8 8 8 8 8 8 a a 3 3 3 1 3 3 a a 
+    8 8 8 8 8 8 a a 3 3 3 3 1 3 a a 
     `, SpriteKind.Player)
 mySprite.setPosition(77, 32)
 controller.moveSprite(mySprite, 100, 100)
+mySprite.setStayInScreen(true)
+info.setLife(3)
+game.onUpdateInterval(1000, function () {
+    ASTEROID = sprites.createProjectileFromSide(img`
+        . . . . . . . . c c c c . . . . 
+        . . . . c c c c c c c c c . . . 
+        . . . c f c c a a a a c a c . . 
+        . . c c f f f f a a a c a a c . 
+        . . c c a f f c a a f f f a a c 
+        . . c c a a a a b c f f f a a c 
+        . c c c c a c c b a f c a a c c 
+        c a f f c c c a b b 6 b b b c c 
+        c a f f f f c c c 6 b b b a a c 
+        c a a c f f c a 6 6 b b b a a c 
+        c c b a a a a b 6 b b a b b a . 
+        . c c b b b b b b b a c c b a . 
+        . . c c c b c c c b a a b c . . 
+        . . . . c b a c c b b b c . . . 
+        . . . . c b b a a 6 b c . . . . 
+        . . . . . . b 6 6 c c . . . . . 
+        `, 0, 50)
+})
